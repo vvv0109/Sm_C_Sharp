cdc10eef2a8d189bfca3d824a20839600e020aac
vvv0109 <112075335+vvv0109@users.noreply.github.com>
2022-09-06 17:59:10 +0300

    Update and rename Sm_3_DZ_3/Program.cs to См_3_ДЗ_3/Program.cs

--- См_3_ДЗ_3/Program.cs
+++ См_3_ДЗ_3/Program.cs
@@ -2,7 +2,4 @@
 Console.Clear(); //Очищаем терминал
 Console.WriteLine("Привет! Покажу тебе таблицу кубов чисел от 1 до N. Введи число:"); //Выводим смысл
 int numA = Convert.ToInt32(Console.ReadLine ()); // Вводим число
-//int index=0;
-//int numE=1;
-//int numZ=0;
 for (int index=0, numZ=0, numE=1; index<numA; numZ=numE*numE*numE, index++, numE++, Console.WriteLine(numZ));
