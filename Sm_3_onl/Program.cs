32a1f89bc5ad3f3c4e9639dff90edd805379d67d
vvv0109 <112075335+vvv0109@users.noreply.github.com>
2022-09-06 16:35:07 +0300

    Add files via upload

---
+++ Sm_3_onl/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear(); //Очищаем терминал
+Console.WriteLine("Привет! Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N."); //Выводим смысл
+int numA = Convert.ToInt32(Console.ReadLine ()); // Вводим число Х точка1
+int index=0;
+int numR=2;
+while (index<numA)
+{
+    numR=index*index;
+    index++;
+    Console.WriteLine (numR);
+}
+
