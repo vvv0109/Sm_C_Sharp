9c8207def88c43a8bf8d14a7e498b80c875b6fc3
vvv0109 <112075335+vvv0109@users.noreply.github.com>
2022-09-09 16:00:57 +0300

    Delete Sm_3_DZ_3 directory

--- Sm_3_DZ_3/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
-Console.Clear(); //Очищаем терминал
-Console.WriteLine("Привет! Покажу тебе таблицу кубов чисел от 1 до N. Введи число:"); //Выводим смысл
-int numA = Convert.ToInt32(Console.ReadLine ()); // Вводим число
-
-for (int index=0, numZ=0, numE=1; index<numA; numZ=numE*numE*numE, index++, numE++, Console.WriteLine(numZ));
