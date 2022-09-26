1657fe946bb3f521ef0355f663f00bd9978f0d3e
vvv0109 <112075335+vvv0109@users.noreply.github.com>
2022-09-26 18:14:58 +0300

    готов

---
+++ Sm_8_DZ_4/Program.cs
@@ -0,0 +1,20 @@
+﻿//Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+//Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+//Массив размером 2 x 2 x 2
+Console.Clear(); //Очищаем терминал
+int numA=new Random().Next(3,10);
+int numF=numA, numC=numA, numD=0;
+int[,,] matrix = new int[numA, numF, numC];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int z = 0; z < matrix.GetLength(1); z++)
+        {
+            Console.WriteLine();
+            for (int w = 0; w < matrix.GetLength(2); w++)
+            {
+                numD=(new Random().Next(10,100));//интервал чисел      
+                matrix[i, z, w]=numD;
+                Console.Write($"{matrix[i, z, w]}({i},{z},{w}) ");
+            }
+        }
+    }
---
+++ Sm_8_DZ_5/Program.cs
@@ -0,0 +1,29 @@
+﻿//Напишите программу, которая заполнит спирально массив 4 на 4.
+Console.Clear(); //Очищаем терминал
+int numA=new Random().Next(4,5);
+int numC=numA;
+int[,] matrix = new int[numA, numC];
+int numD=0, w=0;
+for (int i=0; i<matrix.GetLength(0); i++)
+{    Console.Write("[ ");
+    for (int z=0; z<matrix.GetLength(1) && i==0; z++)
+    {
+        numD++;//интервал чисел      
+        matrix[i,z]=numD;
+    }
+    w=i;
+    for (int z=0; z<matrix.GetLength(0); z++)
+    {
+        matrix[i,numC-1]=numD;
+       Console.Write($"{matrix[i,z]} ");// выводим элемент массива
+    }
+    numD++;
+    i=w;
+    for (int z=0; z<matrix.GetLength(1); z++)
+    {
+        matrix[z,numC-1]=numD;
+    }
+      
+   Console.Write("]");
+   Console.WriteLine();
+}
