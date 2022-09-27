12ce9c9d5b5d1affe60f44ab615ac1cae59090fb
vvv0109 <112075335+vvv0109@users.noreply.github.com>
2022-09-27 15:20:38 +0300

    готов

---
+++ Sm_9_DZ_1/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+//Выполнить с помощью рекурсии.
+Console.Clear(); //Очищаем терминал
+int numA=new Random().Next(5,5);
+int numB (int n)
+{
+if(n>numA) return numA;
+else return n-1;
+}
+for (int i=numA+1, r=1; r<i; i--)
+Console.Write(numB(i));
---
+++ Sm_9_DZ_2/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+Console.Clear(); //Очищаем терминал
+int numA=new Random().Next(1,1);
+int numC=new Random().Next(15,15);
+void numB (int sum)
+{
+if (numA > numC) 
+{
+    Console.WriteLine(sum); 
+    return;
+}
+    sum = sum + (numA++);
+    numB(sum);
+}
+numB(0);
